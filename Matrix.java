c599ba8b54207f852f70feb7132f20e73b73c120
divyasom <50879069+divyasom@users.noreply.github.com>
2019-05-22 19:55:06 -0400

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,37 +1,45 @@
 /** 
  * Generic version of the Matrix class. 
- * @param <T> the type of value stored in the Matrix 
  */ 
-public class Matrix<T> {
+public class Matrix {
 
-	private T[][] matrix; 
+    private Integer[][] matrix; 
 
-	/**
-	 * Instantiates a new matrix 
-	 *
-	 * @param rows the number of rows in the matrix 
-	 * @param cols the number of columns in the matrix 
-	 */
-	public Matrix(int rows, int cols) {
-		matrix = (T[][]) new Object[rows][cols];
-	} 
-	
-	/** 
-	 * Returns the matrix 
-	 * @return the matrix 
-	 */ 
-	public T[][] getMatrix() {
-		return matrix;
-	}
+    /**
+     * Instantiates a new matrix 
+     *
+     * @param rows the number of rows in the matrix 
+     * @param cols the number of columns in the matrix 
+     */
+    public Matrix(int rows, int cols) {
+        matrix = new Integer[rows][cols];
+    } 
+    
+    /**
+     * Instantiates a new matrix 
+     *
+     * @param matrix the matrix 
+     */
+    public Matrix(Integer[][] matrix) {
+        this.matrix = matrix;
+    }
+     
+    /** 
+     * Returns the matrix 
+     * @return the matrix 
+     */ 
+    public Integer[][] getMatrix() {
+        return matrix;
+    }
 
-	/** 
-	 * Update the entry at row r and column c 
-	 *
-	 * @param r the row of the interest element 
-	 * @param c the column of the interest element
-	 * @param v the new value 
-	 */
-	public void updateEntry(int r, int c, T v) {
-		matrix[r][c] = v;
-	}
+    /** 
+     * Update the entry at row r and column c 
+     *
+     * @param r the row of the interest element 
+     * @param c the column of the interest element
+     * @param v the new value 
+     */
+    public void updateEntry(int r, int c, Integer v) {
+        matrix[r][c] = v;
+    }
 }
